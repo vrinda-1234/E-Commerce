29240a063d7f7f1e47b925ed40684856581e8395
SVA <delta@email.com>
2025-03-10 11:42:24 +0530

    Added cart routes and payment routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser"; //to access cookie
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js"
 
 import {connectDB} from "./lib/db.js";
 dotenv.config(); //allows you to read the content of env file
@@ -19,6 +21,8 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/coupons",couponRoutes);
+app.use("/api/payments",paymentRoutes);
 
 
 app.listen(PORT,() =>{
