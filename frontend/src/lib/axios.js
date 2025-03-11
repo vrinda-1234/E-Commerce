be7c832da6b89ff76cd39a96a35547a09126fbf1
vrinda-1234 <163041090+vrinda-1234@users.noreply.github.com>
2025-03-12 01:40:45 +0530

    Updated files

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+const axiosInstance=axios.create({
+    baseURL:import.meta.mode==="development"?"http://localhost:8000/api":"/api",
+    withCredentials:true,
+})
+export default axiosInstance;
